036b020c1ac7e38d0fffec2ee4c377480b03201a
Mohamad Reza <emargi.mail@gmail.com>
2025-01-18 20:20:08 +0330

    added a docstring, updated the versions

--- allsafe/modules/encryption.py
+++ allsafe/modules/encryption.py
@@ -39,13 +39,17 @@ def _get_steps_based_on_length(cipher_len, passwd_len) -> int:
     return cipher_len // passwd_len
 
 def _convert_hex_to_list_of_ints(hex_string: str, length: int) -> list[int]:
+    """
+    This function will take a hexadecimal number (`hex_string`) that will be used to generate
+    numbers as much as specified in `length` (length of the result list) parameter.
+    """
     nums = []
     cipher_len = len(hex_string)
     steps = _get_steps_based_on_length(cipher_len, length)
     for i in range(0, cipher_len, steps):
         nums.append(int(hex_string[i::2], base=16))
     # `hex_string` might not be divisible by `length`, and
-    # that results in longer nums than the given `length`
+    # that results in longer `nums` than the given `length`
     # this is a compatible option, for now.
     return nums[:length]
 
